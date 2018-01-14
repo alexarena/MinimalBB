9250cbfd97a0fcc526eda0d04b8c77e4c04db721
Alex Arena <im@alexarena.com>
2018-01-14 16:08:04 -0700

    initial commit

---
+++ MinimalBB.safariextension/minimalbb.js
@@ -0,0 +1,30 @@
+const hideElements = (els) => {
+  for(let el of els){
+    const elObj = document.getElementById(el)
+    if(elObj){
+      elObj.style.display = "none"
+    }
+  }
+}
+
+hideElements([
+  'module:_868_1',
+  'module:_351_1',
+  'actionbar',
+  'quick_links_wrap',
+  'asu_nav_wrapper',
+  'column0',
+  'topframe.logout.label',
+  'global-avatar'
+])
+
+document.getElementsByClassName('tabWrapper-left')[0].style.visibility = 'hidden'
+
+const headImg = document.getElementById('topTabs').querySelectorAll('[target][title]')[0]
+headImg.href = 'https://myasucourses.asu.edu/'
+headImg.setAttribute('target','')
+
+let tmp = document.getElementById('column1')
+if(tmp){
+  tmp.style.width = '60%'
+}
